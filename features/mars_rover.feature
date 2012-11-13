1c4f1a8f95ff22ff78a438f08bdc934715652301
Oliver Martell <oliver.martell@gmail.com>
2012-11-13 21:49:23 +0000

    Include orientation in tests

--- features/mars_rover.feature
+++ features/mars_rover.feature
@@ -7,16 +7,16 @@ Scenario: Deploying a rover to mars surface
   When I indicate that I want to start the expedition
   Then I should be asked the size of the area to explore
   And I should see that the area to explore is 3x3
-  And that a rover is ready to receive instructions at 0,0
+  And that a rover is ready to receive instructions at 0,0,'N'
 
 Scenario: Moving the rover forward
   Given there's a 3x3 recognized area to explore in Mars
   And I have a rover at the initial position
   When I send the 'F' to the rover
-  Then the rover should be in position 0,1
+  Then the rover should be in position 0,1,'N'
 
 Scenario: Moving the rover to the right
   Given there's a 3x3 recognized area to explore in Mars
   And I have a rover at the initial position
   When I send the 'R' to the rover
-  Then the rover should be in position 1,0
+  Then the rover should be in position 1,0,'N'
--- features/step_definitions/mars_rover_steps.rb
+++ features/step_definitions/mars_rover_steps.rb
@@ -35,34 +35,34 @@ Then /^I should see that the area to explore is (\d+)x(\d+)$/ do |width, height|
   area_to_explore.should eq [3,3]
 end
 
-Then /^that a rover is ready to receive instructions at (\d+),(\d+)$/ do |x, y|
+Then /^that a rover is ready to receive instructions at (\d+),(\d+),'(.)'$/ do |x, y, orientation|
   io.outputs.should include "Please provide instructions:"
-  rovers.should eq [[0,0]]
+  rovers.should eq [[ 0, 0, 'N']]
 end
 
 Given /^I have a rover at the initial position$/ do
-  rovers << [0,0]
+  rovers << [0, 0, 'N']
 end
 
-Then /^the rover should be in position (\d+),(\d+)$/ do |x, y|
-  rovers.should eq [[x.to_i,y.to_i]]
+Then /^the rover should be in position (\d+),(\d+),'(.)'$/ do |x, y, orientation|
+  rovers.should eq [[x.to_i,y.to_i, orientation]]
 end
 
 When /^I send the 'F' to the rover$/ do
   x,y = rovers.shift
-  rovers << [x, y + 1]
+  rovers << [x, y + 1, 'N']
 end
 
 When /^I send the 'R' to the rover$/ do
   x,y = rovers.shift
-  rovers << [x + 1, y]
+  rovers << [x + 1, y, 'N']
 end
 
 def start_expedition
   io.puts("What is the size of the area that you would like to explore:")
   area_to_explore(io.gets.split("x").map(&:to_i))
   io.puts("Please provide instructions:")
-  rovers << [0,0]
+  rovers << [0,0, 'N']
 end
 
 def area_to_explore(area = nil)
