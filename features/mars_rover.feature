107cd0acf51525e32103019cc06cb348ef582d52
Oliver Martell <oliver.martell@gmail.com>
2012-11-13 21:21:34 +0000

    Make second test pass

--- features/mars_rover.feature
+++ features/mars_rover.feature
@@ -7,4 +7,10 @@ Scenario: Deploying a rover to mars surface
   When I indicate that I want to start the expedition
   Then I should be asked the size of the area to explore
   And I should see that the area to explore is 3x3
-  And that the rover should be ready to receive instructions
+  And that a rover is ready to receive instructions at 0,0
+
+Scenario: Moving the rover forward
+  Given there's a 3x3 recognized area to explore in Mars
+  And I have a rover at the initial position
+  When I send the forward instruction to the rover
+  Then the rover should be in position 0,1
--- features/step_definitions/mars_rover_steps.rb
+++ features/step_definitions/mars_rover_steps.rb
@@ -39,8 +39,30 @@ Then /^that the rover should be ready to receive instructions$/ do
   io.outputs.should include "Please provide instructions:"
 end
 
+Then /^that a rover is ready to receive instructions at (\d+),(\d+)$/ do |x, y|
+  rovers.should eq [[0,0]]
+end
+
+Given /^I have a rover at the initial position$/ do
+  rovers << [0,0]
+end
+
+When /^I send the forward instruction to the rover$/ do
+  x,y = rovers.shift
+  rovers << [x, y+1]
+end
+
+Then /^the rover should be in position (\d+),(\d+)$/ do |x, y|
+  rovers.should eq [[x.to_i,y.to_i]]
+end
+
 def start_expedition
   io.puts("What is the size of the area that you would like to explore:")
   io.gets
   io.puts("Please provide instructions:")
+  rovers << [0,0]
+end
+
+def rovers
+  @rovers ||= []
 end
