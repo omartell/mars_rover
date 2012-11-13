9dd595711f096a16ec0c24f832dd71a0de53a6fc
Oliver Martell <oliver.martell@gmail.com>
2012-11-13 22:19:25 +0000

    Separate tests that ask for user input

--- features/mars_rover.feature
+++ features/mars_rover.feature
@@ -2,11 +2,16 @@ Feature:
   Control the rovers that will find the hidden treasures
   in the mars surface.
 
+Scenario: Prompting the user for inputs
+  Given there's an expedition to Mars
+  When I start the exploration program
+  Then I should be prompted to provide the size of the area to explore
+  And the start position of the rover
+
 Scenario: Deploying a rover to mars surface
-  Given there's a 3x3 recognized area to explore in Mars
-  When I indicate that I want to start the expedition
-  Then I should be asked the size of the area to explore
-  And I should see that the area to explore is 3x3
+  Given there's an area to explore in Mars
+  When I indicate I want to start exploring an area of 3x3 starting at 0,0,'N'
+  Then I should get a confirmation that the area to explore is 3x3
   And that a rover is ready to receive instructions at 0,0,'N'
 
 Scenario: Moving the rover forward
--- features/step_definitions/mars_rover_steps.rb
+++ features/step_definitions/mars_rover_steps.rb
@@ -19,24 +19,42 @@ def io
   @io ||= IO.new
 end
 
-Given /^there's a (\d+)x(\d+) recognized area to explore in Mars$/ do |width, height|
-  io.inputs << "#{width}x#{height}"
+Given /^there's an expedition in Mars$/ do
+  
 end
 
-When /^I indicate that I want to start the expedition$/ do
+Given /^there's an area to explore in Mars$/ do
+
+end
+
+When /^I start the exploration program$/ do
+  io.inputs << "3x3"
   start_expedition
 end
 
-Then /^I should be asked the size of the area to explore$/ do
+Then /^I should be prompted to provide the size of the area to explore$/ do
   io.outputs.should include "What is the size of the area that you would like to explore:"
 end
 
-Then /^I should see that the area to explore is (\d+)x(\d+)$/ do |width, height|
+Then /^the start position of the rover$/ do
+  io.outputs.should include "Please provide instructions for the first rover:"
+end
+
+When /^I indicate I want to start exploring an area of (\d+)x(\d+) starting at (\d+),(\d+),'(.)'$/ do |width, height, start_x, start_y, orientation|
+  io.inputs << "#{width}x#{height}"
+  start_expedition
+end
+
+Then /^I should get a confirmation that the area to explore is (\d+)x(\d+)$/ do |arg1, arg2|
   area_to_explore.should eq [3,3]
 end
 
+Given /^there's a (\d+)x(\d+) recognized area to explore in Mars$/ do |width, height|
+  
+end
+
 Then /^that a rover is ready to receive instructions at (\d+),(\d+),'(.)'$/ do |x, y, orientation|
-  io.outputs.should include "Please provide instructions:"
+  io.outputs.should include "Please provide instructions for the first rover:"
   rovers.should eq [[ 0, 0, 'N']]
 end
 
@@ -61,7 +79,7 @@ end
 def start_expedition
   io.puts("What is the size of the area that you would like to explore:")
   area_to_explore(io.gets.split("x").map(&:to_i))
-  io.puts("Please provide instructions:")
+  io.puts("Please provide instructions for the first rover:")
   rovers << [0,0, 'N']
 end
 
